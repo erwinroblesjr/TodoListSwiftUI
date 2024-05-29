bca73d6902db2c7c6c5e59b0c247e5b8bc184308
erwinroblesjr <68773098+erwinroblesjr@users.noreply.github.com>
2024-05-28 22:27:43 -0400

    Add initial files

---
+++ TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by Erwin Robles on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
